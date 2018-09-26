9d8498fcdcac3aa039213b0b90271156cc2ef247
decanus <dean.eigenmann@icloud.com>
2018-09-26 12:48:09 +0200

    added DNSSEC

--- contracts/Root.sol
+++ contracts/Root.sol
@@ -1,14 +1,17 @@
 pragma solidity ^0.4.24;
 
 import "@ensdomains/ens/contracts/ENS.sol";
+import "@ensdomains/dnssec-oracle/contracts/DNSSEC.sol";
 import "./Ownable.sol";
 
 contract Root is Ownable {
 
     ENS public ens;
+    DNSSEC public dnssec;
 
-    constructor(ENS _ens) public {
+    constructor(ENS _ens, DNSSEC _dnssec) public {
         ens = _ens;
+        dnssec = _dnssec;
     }
 
 }
