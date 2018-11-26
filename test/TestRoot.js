8bb56ae749f1018f96a1cf7aa50771efd7406cad
decanus <dean.eigenmann@icloud.com>
2018-11-26 18:43:47 +0900

    minor updates

--- test/TestRoot.js
+++ test/TestRoot.js
@@ -2,33 +2,32 @@ const Root = artifacts.require('./Root.sol');
 const DNSSEC = artifacts.require('./mocks/DummyDNSSEC.sol');
 const ENS = artifacts.require('./ENSRegistry.sol');
 const utils = require('./helpers/Utils.js');
-const sha3 = require('js-sha3').keccak_256;
+const namehash = require('eth-ens-namehash');
 
 contract('Root', function(accounts) {
 
     let ens, dnssec, root;
 
-    let tld = '0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563';
-
     beforeEach(async function() {
+
         ens = await ENS.new();
         dnssec = await DNSSEC.new();
         root = await Root.new(ens.address, dnssec.address);
 
-        await ens.setSubnodeOwner(0, tld, root.address);
+        await ens.setSubnodeOwner(0, web3.sha3('eth'), root.address, {from: accounts[0]});
+        await ens.setOwner(0, root.address);
     });
 
     it('should fail when trying to set subnode owner for non root domain', async () => {
         try {
-            await root.setSubnodeOwner(0, tld, accounts[1]);
+            await root.setSubnodeOwner(web3.sha3('eth'), '0x123', accounts[1], {from: accounts[0]});
         } catch (error) {
             return utils.ensureException(error);
         }
     });
 
-    // @todo why does this not work?
-    // it('should allow setting subnode when trying to owner for root domain', async () => {
-    //     await root.setSubnodeOwner(tld, '0xdead', accounts[1]);
-    // });
+    it('should allow setting subnode when trying to owner for root domain', async () => {
+        await root.setSubnodeOwner(0x0, web3.sha3('eth'), accounts[1], {from: accounts[0]});
+    });
 
 });
