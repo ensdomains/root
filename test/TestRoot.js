ed99270dd802d1c382656fd8d31c753647c9b7c4
decanus <dean.eigenmann@icloud.com>
2018-11-26 22:43:53 +0900

    updated, put into describe, using asserts

--- test/TestRoot.js
+++ test/TestRoot.js
@@ -1,13 +1,17 @@
 const Root = artifacts.require('./Root.sol');
 const DNSSEC = artifacts.require('./mocks/DummyDNSSEC.sol');
 const ENS = artifacts.require('./ENSRegistry.sol');
+
 const utils = require('./helpers/Utils.js');
+const namehash = require('eth-ens-namehash');
 
 contract('Root', function(accounts) {
 
+    let node;
     let ens, dnssec, root;
 
     beforeEach(async function() {
+        node = namehash.hash('eth');
 
         ens = await ENS.new();
         dnssec = await DNSSEC.new();
@@ -17,16 +21,20 @@ contract('Root', function(accounts) {
         await ens.setOwner(0, root.address);
     });
 
-    it('should fail when trying to set subnode owner for non root domain', async () => {
-        try {
-            await root.setSubnodeOwner(web3.sha3('eth'), '0x123', accounts[1], {from: accounts[0]});
-        } catch (error) {
-            return utils.ensureException(error);
-        }
-    });
+    describe('setSubnodeOwner', async () => {
 
-    it('should allow setting subnode when trying to owner for root domain', async () => {
-        await root.setSubnodeOwner(0, web3.sha3('eth'), accounts[1], {from: accounts[0]});
+        it('should fail when trying to set subnode owner for non root domain', async () => {
+            try {
+                await root.setSubnodeOwner(web3.sha3('eth'), '0x123', accounts[1], {from: accounts[0]});
+            } catch (error) {
+                return utils.ensureException(error);
+            }
+        });
+
+        it('should allow setting subnode when trying to owner for root domain', async () => {
+            await root.setSubnodeOwner(0, web3.sha3('eth'), accounts[1], {from: accounts[0]});
+            assert.equal(accounts[1], await ens.owner(node));
+        });
     });
 
 });
