21af39871b14646af9addfc1955b1753000b7229
decanus <dean.eigenmann@icloud.com>
2019-01-08 21:35:11 +0100

    removed

--- test/TestRoot.js
+++ test/TestRoot.js
@@ -123,8 +123,8 @@ contract('Root', function(accounts) {
             assert.equal(await ens.owner(namehash.hash('test')), "0x0000000000000000000000000000000000000000");
         });
 
-        it.only('should set TLD owner to default registrar when none is provided', async () => {
-            let proof = '0x00000000000000000123';
+        it('should set TLD owner to default registrar when none is provided', async () => {
+            let proof = 0;
 
             await dnssec.setData(
                 16,
