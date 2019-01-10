395afbb54fda7e6fe05f937b230b415114f86785
decanus <dean.eigenmann@icloud.com>
2019-01-10 23:29:22 +0100

    cleaned up oracle for more types

--- test/mocks/DummyDNSSEC.sol
+++ test/mocks/DummyDNSSEC.sol
@@ -4,28 +4,28 @@ import "@ensdomains/ens/contracts/ENSRegistry.sol";
 
 contract DummyDNSSEC {
 
-    uint16 expectedType;
-    bytes expectedName;
-    uint32 inception;
-    uint64 inserted;
-    bytes20 hash;
+    struct Data {
+        uint32 inception;
+        uint64 inserted;
+        bytes20 hash;
+    }
+
+    mapping (bytes32 => Data) private datas;
 
     function setData(uint16 _expectedType, bytes _expectedName, uint32 _inception, uint64 _inserted, bytes _proof) public {
-        expectedType = _expectedType;
-        expectedName = _expectedName;
-        inception = _inception;
-        inserted = _inserted;
+        Data storage rr = datas[keccak256(_expectedType, _expectedName)];
+        rr.inception = _inception;
+        rr.inserted = _inserted;
 
         if (_proof.length != 0) {
-            hash = bytes20(keccak256(_proof));
+            rr.hash = bytes20(keccak256(_proof));
         } else {
-            hash = bytes20(0);
+            rr.hash = bytes20(0);
         }
     }
 
-    function rrdata(uint16 dnstype, bytes name) public view returns(uint32, uint64, bytes20) {
-        require(dnstype == expectedType);
-        require(keccak256(name) == keccak256(expectedName));
-        return (inception, inserted, hash);
+    function rrdata(uint16 dnstype, bytes name) public view returns (uint32, uint64, bytes20) {
+        Data rr = datas[keccak256(dnstype, name)];
+        return (rr.inception, rr.inserted, rr.hash);
     }
 }
